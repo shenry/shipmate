a2582dd1eeed030bda26880d761f1c377bc7885c
Stuart Henry <stuarthenry@gmail.com>
2008-07-09 23:21:54 -0700

    oops

--- db/migrate/001_create_wineries.rb
+++ db/migrate/001_create_wineries.rb
@@ -2,7 +2,7 @@ class CreateWineries < ActiveRecord::Migration
   def self.up
     create_table :wineries do |t|
       t.string :name,           :limit => 25, :null => false, :default => ""
-      t.integer :bond,                        :null => false, :default => ""
+      t.integer :bond,                        :null => false, :default => 0
       t.string :address_line_1,               :null => false, :default => ""
       t.string :address_line_2,               :null => false, :default => ""
       t.string :city,                         :null => false, :default => ""
